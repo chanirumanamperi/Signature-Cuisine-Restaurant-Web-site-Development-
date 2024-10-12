1fe2e37e99b1cb7c89b93f2265d8e67ff48e8133
Chaniru G Manamperi <chanirumanamperi@gamil.com>
2024-10-12 23:19:27 +0530

    Initial commit

---
+++ user_form.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jul 26, 2023 at 09:15 AM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `user_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_form`
+--
+
+DROP TABLE IF EXISTS `user_form`;
+CREATE TABLE IF NOT EXISTS `user_form` (
+  `ID` int(255) DEFAULT NULL,
+  `Name` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `user_type` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `passwoed` varchar(255) CHARACTER SET utf8mb4 NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
